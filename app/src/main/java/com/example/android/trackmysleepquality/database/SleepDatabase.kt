6decaeb3c9a224faae502d9cee0d795646a23c1b
heroserth2 <59160969@go.buu.ac.th>
2019-10-07 21:12:37 +0700

    Add a ViewModel

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -22,24 +22,31 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
-abstract  class SleepDatabase : RoomDatabase() {
-    abstract val sleepDatabaseDao: SleepDatabaseDao
-    companion object {
-        @Volatile
-        private  var INSTANCE: SleepDatabase? = null
-    }
+abstract class SleepDatabase : RoomDatabase() {
 
-    fun getInstance(context: Context): SleepDatabase {
-        synchronized(this) {
-            var instance = INSTANCE
-            if (instance == null) {
-                instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        SleepDatabase::class.java,
-                        "sleep_history_database").fallbackToDestructiveMigration().build()
-                INSTANCE = instance
-            }
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
                 return instance
+            }
         }
     }
 }
