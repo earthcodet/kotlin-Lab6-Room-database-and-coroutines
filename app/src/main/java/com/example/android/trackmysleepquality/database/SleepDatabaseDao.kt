1e36e4183a974acb8c230099584a16b9f8d08412
heroserth2 <59160969@go.buu.ac.th>
2019-10-07 21:29:40 +0700

    Set upcoroutines for database operations

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -24,25 +24,22 @@ import androidx.room.Update
 
 @Dao
 interface SleepDatabaseDao {
+
     @Insert
     fun insert(night: SleepNight)
 
     @Update
-    fun update(night:SleepNight)
+    fun update(night: SleepNight)
 
-    @Query ("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
     fun get(key: Long): SleepNight?
 
-    @Query ("DELETE FROM daily_sleep_quality_table")
+    @Query("DELETE FROM daily_sleep_quality_table")
     fun clear()
 
-    @Query ("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     fun getTonight(): SleepNight?
 
-    @Query ("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
     fun getAllNights(): LiveData<List<SleepNight>>
-
-
 }
-
-
